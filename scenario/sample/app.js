c24bf3e9d12d7a7edce3873d2a93a1b4560803c9
markjin1990 <markjin1990@gmail.com>
2015-03-19 13:16:12 -0400

    scripts for concurrency tests

--- scenario/sample/app.js
+++ scenario/sample/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var testab = require('./routes/testab');
 
 var app = express();
 
@@ -20,7 +21,8 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
+app.use('/', testab);
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
